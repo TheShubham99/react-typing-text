881e9a7321afd01a0bde414189d05089e2e56391
unknown <ssahasrabhojane@gmail.com>
2020-06-29 23:23:41 +0530

    Final version
    feat: Typing effect / Dynamic Text
    feat: static text
    feat: typing speed
    feat: delay

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-typing-text'
+import ExampleComponent from 'react-typing-text'
 import 'react-typing-text/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <ExampleComponent staticText='$' text='Create React Library Example 😄' />
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,98 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import cx from 'classnames'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export default class SimpleTypingText extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: props.data,
+      index: 0,
+      displayText: ''
+    }
+
+    this.typing = false
+    this.getRawLetters = this.getRawLetters.bind(this)
+    this.type = this.type.bind(this)
+
+    this.beginTyping = this.beginTyping.bind(this)
+  }
+
+  componentDidMount() {
+    this.beginTyping()
+  }
+
+  componentDidUpdate() {
+    if (this.typing === false) {
+      this.beginTyping()
+    }
+    if (this.props.text === this.state.displayText) {
+      this.typing = false
+    }
+  }
+
+  componentWillUnmount() {
+    this._timeout && clearTimeout(this._timeout)
+  }
+
+  beginTyping() {
+    this.typing = true
+    this._timeout = setTimeout(() => {
+      this.type()
+    }, this.props.typingDelay)
+  }
+
+  getRawLetters() {
+    const { text } = this.props
+    return typeof text === 'string' ? [text] : [...text]
+  }
+
+  type() {
+    let { index, displayText } = this.state
+    let text = this.getRawLetters()[index]
+    if (text.length !== displayText.length) {
+      displayText = text.substr(0, displayText.length + 1)
+      this.setState({ displayText }, () => {
+        this._timeout = setTimeout(() => {
+          this.type()
+        }, this.props.speed)
+      })
+    }
+  }
+
+  render() {
+    const {
+      speed,
+      typingDelay,
+      staticText,
+      text,
+      className,
+      ...otherProps
+    } = this.props
+    const { displayText } = this.state
+    const classes = cx(className, 'ps_typing')
+    return (
+      <span {...otherProps} className={classes}>
+        {staticText ? (
+          <span className='ps_typing_static_text'>{staticText}&nbsp;</span>
+        ) : null}
+
+        <span className='ps_typing_text'>{displayText}</span>
+      </span>
+    )
+  }
+}
+
+SimpleTypingText.defaultProps = {
+  speed: 100,
+  typingDelay: 500
+}
+
+SimpleTypingText.propTypes = {
+  speed: PropTypes.number.isRequired,
+  typingDelay: PropTypes.number.isRequired,
+
+  staticText: PropTypes.string,
+  text: PropTypes.oneOfType([PropTypes.array, PropTypes.string]).isRequired,
+  className: PropTypes.string
 }
